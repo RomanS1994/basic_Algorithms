aa051741f86fb9426364e4855b1bacb237c11489
Roman Stryzhka <rstrizhko1994@gmail.com>
2025-01-22 20:24:42 +0100

    home practice 22.01.2025

---
+++ graphs/test_analiz.py
@@ -0,0 +1,21 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+G = nx.Graph()
+
+G.add_node("A")
+G.add_nodes_from(["B", "C", "D"])
+
+G.add_edge("A", "B")
+G.add_edges_from([("A", "C"), ("B", "C"), ("B", "D")])
+
+num_nodes = G.number_of_nodes()  # 4
+num_edges = G.number_of_edges()  # 4
+is_connected = nx.is_connected(G)  # True
+pos = nx.circular_layout(G)
+
+nx.draw(G,pos, with_labels=True)
+
+plt.title("Circular Layout")
+plt.show()
